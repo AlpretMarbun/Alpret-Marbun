4ec326aa7fbedf89bf763144613ab75d46db912e
AlpretMarbun <alpretjr56@gmail.com>
2025-10-22 21:12:19 +0700

    Create matriks.py

---
+++ matriks.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+A = np.array([[2, 3], [4, 5], [6, 7]])
+B = np.array([[1, 2], [3, 4], [5, 6]])
+print("Matriks A:")
+print(A)
+print("\nMatriks B:")
+print(B)
+result_addition = matrix_addition(A, B)
+print("\nHasil Penjumlahan Matriks A dan B:")
+print(result_addition)
